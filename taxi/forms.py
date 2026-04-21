d9b5dd80db7aa78c15c9acb5eb8fd6690107dbd4
Your Name <you@example.com>
2026-04-21 20:46:44 +0300

    Fix some issues

--- taxi/forms.py
+++ taxi/forms.py
@@ -1,14 +1,17 @@
 from django import forms
-from django.contrib.admin.widgets import FilteredSelectMultiple
 from django.core.exceptions import ValidationError
-
-from taxi.models import Driver, Car
+from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
+
+from taxi.models import Car
+
+
+User = get_user_model()
 
 
 class CarForm(forms.ModelForm):
     drivers = forms.ModelMultipleChoiceField(
-        queryset=Driver.objects.all(),
+        queryset=User.objects.all(),
         widget=forms.CheckboxSelectMultiple,
         required=False,
     )
@@ -20,7 +23,7 @@ class CarForm(forms.ModelForm):
 
 class DriverCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
-        model = Driver
+        model = User
         fields = UserCreationForm.Meta.fields + (
             "first_name",
             "last_name",
@@ -29,16 +32,20 @@ class DriverCreationForm(UserCreationForm):
         )
 
     def clean_license_number(self):
-        return validate_license_number(self.cleaned_data.get("license_number"))
+        return validate_license_number(
+            self.cleaned_data.get("license_number")
+        )
 
 
 class DriverLicenseUpdateForm(forms.ModelForm):
     class Meta:
-        model = Driver
+        model = User
         fields = ("license_number",)
 
     def clean_license_number(self):
-        return validate_license_number(self.cleaned_data.get("license_number"))
+        return validate_license_number(
+            self.cleaned_data.get("license_number")
+        )
 
 
 def validate_license_number(license_number):
@@ -51,9 +58,13 @@ def validate_license_number(license_number):
         raise ValidationError("License must be exactly 8 characters")
 
     if not license_number[:3].isupper() or not license_number[:3].isalpha():
-        raise ValidationError("First 3 characters must be uppercase letters")
+        raise ValidationError(
+            "First 3 characters must be uppercase letters"
+        )
 
     if not license_number[3:].isdigit():
-        raise ValidationError("Last 5 characters must be digits")
+        raise ValidationError(
+            "Last 5 characters must be digits"
+        )
 
     return license_number
